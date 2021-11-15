fa4108561c53ac0e596f69d83aabeb0a275c6f89
answlsn1 <94333414+answlsn1@users.noreply.github.com>
2021-11-15 12:50:01 +0900

    기본코드, 이미지 업로드
    기본코드, 이미지 업로드

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    // 베스트 상품 메뉴 
+    var best_cate_list = $('.best-cate-list>li');
+    var best_cate_bg = $('.best-cate-bg');
+    var distanceY = 55;
+    $.each(best_cate_list, function (index, item) {
+        var aTag = $(this).find('>a');
+        var nowLi = $(this);
+        aTag.click(function (e) {
+            e.preventDefault();
+            best_cate_bg.css('top', index * distanceY);
+            best_cate_list.removeClass('best-cate-focus');
+            nowLi.addClass('best-cate-focus');
+        });
+    });
+
+    // 배너슬라이드
+    new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        effect: "fade",
+        crossEffect: {
+            crossFade: true
+        },
+        speed: 1000,
+    });
+
+    // 펼침 목록 만들기
+    $('.f-site-bt').click(function(){
+       $('.f-site-list').stop().slideToggle(); 
+       $('.f-site-bt').toggleClass('f-site-bt-active');
+    });
+
+});
