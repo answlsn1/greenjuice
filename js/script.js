5e5c5aa2a6f243fe4d6564bc0ca0ddf78f42cc0e
answlsn1 <94333414+answlsn1@users.noreply.github.com>
2021-11-18 11:26:43 +0900

    modal 추가
    modal 추가

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
 $(document).ready(function () {
+
+    // 모달창
+    let modal = $('.modal');
+    let modal_cont = $('.modal-cont');
+    let modal_close = $('.modal-close');
+
+    modal.fadeIn();
+
+    modal_close.click(function () {
+        modal.fadeOut();
+    });
+
+    modal.click(function () {
+        modal.fadeOut();
+    });
+
+    modal_cont.click(function (event) {
+        event.stopPropagation();
+    });
+
+
+
     // 베스트 상품 메뉴 
     var best_cate_list = $('.best-cate-list>li');
     var best_cate_bg = $('.best-cate-bg');
@@ -29,9 +51,9 @@ $(document).ready(function () {
     });
 
     // 펼침 목록 만들기
-    $('.f-site-bt').click(function(){
-       $('.f-site-list').stop().slideToggle(); 
-       $('.f-site-bt').toggleClass('f-site-bt-active');
+    $('.f-site-bt').click(function () {
+        $('.f-site-list').stop().slideToggle();
+        $('.f-site-bt').toggleClass('f-site-bt-active');
     });
 
 });
